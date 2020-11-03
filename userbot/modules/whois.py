1e2349eb43044146faafd4e376d57bd9db7d6149
DarkoBunny <73769985+Ajibcdefgh@users.noreply.github.com>
2020-11-03 23:23:31 +0700

    Update whois.py

--- userbot/modules/whois.py
+++ userbot/modules/whois.py
@@ -21,7 +21,7 @@ from userbot.events import register
 async def who(event):
 
     await event.edit(
-        "`Sit tight while I steal some data from *Global Network Zone*...`"
+        "`Tunggu saya sedang mencuri data dari Emak...`"
     )
 
     if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
@@ -129,19 +129,19 @@ async def fetch_info(replied_user, event):
     username = "@{}".format(username) if username else ("This User has no Username")
     user_bio = "This User has no About" if not user_bio else user_bio
 
-    caption = "<b>USER INFO:</b>\n\n"
-    caption += f"First Name: {first_name}\n"
-    caption += f"Last Name: {last_name}\n"
-    caption += f"Username: {username}\n"
-    caption += f"Data Centre ID: {dc_id}\n"
-    caption += f"Number of Profile Pics: {replied_user_profile_photos_count}\n"
-    caption += f"Is Bot: {is_bot}\n"
-    caption += f"Is Restricted: {restricted}\n"
-    caption += f"Is Verified by Telegram: {verified}\n"
-    caption += f"ID: <code>{user_id}</code>\n\n"
-    caption += f"Bio: \n<code>{user_bio}</code>\n\n"
-    caption += f"Common Chats with this user: {common_chat}\n"
-    caption += "Permanent Link To Profile: "
+    caption = "<b>🕵️USER INFO:</b>\n\n"
+    caption += f"🗣️First Name: {first_name}\n"
+    caption += f"🗣️Last Name: {last_name}\n"
+    caption += f"👤Username: {username}\n"
+    caption += f"🏫Data Centre ID: {dc_id}\n"
+    caption += f"🖼️Number of Profile Pics: {replied_user_profile_photos_count}\n"
+    caption += f"🤖Is Bot: {is_bot}\n"
+    caption += f"🚫Is Restricted: {restricted}\n"
+    caption += f"☑️Is Verified by Telegram: {verified}\n"
+    caption += f"🆔ID: <code>{user_id}</code>\n\n"
+    caption += f"📝Bio: \n<code>{user_bio}</code>\n\n"
+    caption += f"💬Common Chats with this user: {common_chat}\n"
+    caption += "🔗Permanent Link To Profile: "
     caption += f'<a href="tg://user?id={user_id}">{first_name}</a>'
 
     return photo, caption
@@ -149,7 +149,7 @@ async def fetch_info(replied_user, event):
 
 CMD_HELP.update(
     {
-        "whois": ">`.whois <username> or reply to someones text with .whois`"
-        "\nUsage: Gets info of an user."
+        "whois": ">`.whois <username> atay balas ke pesan seseorang dengan .whois`"
+        "\nUsage: Membongkar kedok user."
     }
 )
