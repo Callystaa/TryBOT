b22b1cee01484ced43d612ad99e0968cc496fd39
DarkoBunny <73769985+Ajibcdefgh@users.noreply.github.com>
2020-11-05 22:36:58 +0700

    Translate again

--- userbot/modules/telegraph.py
+++ userbot/modules/telegraph.py
@@ -22,7 +22,7 @@ auth_url = r["auth_url"]
 @register(outgoing=True, pattern="^.tg (m|t)$")
 async def telegraphs(graph):
     """For .telegraph command, upload media & text to telegraph site."""
-    await graph.edit("`Processing...`")
+    await graph.edit("`Proses...`")
     if not graph.text[0].isalpha() and graph.text[0] not in ("/", "#", "@", "!"):
         if graph.fwd_from:
             return
