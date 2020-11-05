6aab00342c38048b433f10354a34be686911c80a
DarkoBunny <73769985+Ajibcdefgh@users.noreply.github.com>
2020-11-05 22:33:03 +0700

    Translate again and again

--- userbot/modules/lyrics.py
+++ userbot/modules/lyrics.py
@@ -21,9 +21,9 @@ if GENIUS is not None:
 
 @register(outgoing=True, pattern=r"^\.lyrics (?:(now)|(.*) - (.*))")
 async def lyrics(lyric):
-    await lyric.edit("`Getting information...`")
+    await lyric.edit("`Mendapatkan informasi...`")
     if GENIUS is None:
-        await lyric.edit("`Provide genius access token to Heroku ConfigVars...`")
+        await lyric.edit("`Berikan token akses genius ke Heroku ConfigVars...`")
         return False
     if lyric.pattern_match.group(1) == "now":
         playing = User(LASTFM_USERNAME, lastfm).get_now_playing()
@@ -35,15 +35,15 @@ async def lyrics(lyric):
     else:
         artist = lyric.pattern_match.group(2)
         song = lyric.pattern_match.group(3)
-    await lyric.edit(f"`Searching lyrics for {artist} - {song}...`")
+    await lyric.edit(f"`Mencari lirik {artist} - {song}...`")
     songs = genius.search_song(song, artist)
     if songs is None:
-        await lyric.edit(f"`Song`  **{artist} - {song}**  `not found...`")
+        await lyric.edit(f"`Lagu`  **{artist} - {song}**  `tidak ditemukan...`")
         return False
     if len(songs.lyrics) > 4096:
         await lyric.edit("`Lyrics is too big, pasted lyrics to Nekobin. please wait.`")
         with open("lyrics.txt", "w+") as f:
-            f.write(f"Search query: \n{artist} - {song}\n\n{songs.lyrics}")
+            f.write(f"Kueri penelusuran: \n{artist} - {song}\n\n{songs.lyrics}")
         lirik = codecs.open("lyrics.txt", "r", encoding="utf-8")
         data = lirik.read()
         key = (
@@ -57,7 +57,7 @@ async def lyrics(lyric):
         os.remove("lyrics.txt")
     else:
         await lyric.edit(
-            f"**Search query**:\n`{artist}` - `{song}`" f"\n\n```{songs.lyrics}```"
+            f"**Kueri penelusuran**:\n`{artist}` - `{song}`" f"\n\n```{songs.lyrics}```"
         )
 
     return True
@@ -66,8 +66,8 @@ async def lyrics(lyric):
 CMD_HELP.update(
     {
         "lyrics": ">`.lyrics` **<artist name> - <song name>**"
-        "\nUsage: Get lyrics matched artist and song."
+        "\nUsage: Dapatkan lirik yang cocok dengan artis dan lagu."
         "\n\n>`.lyrics now`"
-        "\nUsage: Get lyrics artist and song from current lastfm scrobbling."
+        "\nUsage: Dapatkan lirik artis dan lagu dari scrobbling lastfm saat ini."
     }
 )
