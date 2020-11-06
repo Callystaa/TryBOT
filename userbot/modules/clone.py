aee3c8614431e8b45906b95861b72fb1c9b8509c
DarkoBunny <73769985+Ajibcdefgh@users.noreply.github.com>
2020-11-06 05:29:03 +0000

    pylint: auto fixes

--- userbot/modules/clone.py
+++ userbot/modules/clone.py
@@ -1,14 +1,15 @@
-#Copy That Plugin by @ViperAdnan
-#Give credit if you are going to kang it.
+# Copy That Plugin by @ViperAdnan
+# Give credit if you are going to kang it.
 
 import html
-import os
-from telethon.tl.functions.photos import GetUserPhotosRequest
+
+from telethon.tl import functions
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import MessageEntityMentionName
-from telethon.tl import functions
-from userbot import CMD_HELP, bot, TEMP_DOWNLOAD_DIRECTORY
+
+from userbot import CMD_HELP, TEMP_DOWNLOAD_DIRECTORY, bot
 from userbot.events import register
+
 
 @register(outgoing=True, pattern="^\.clone ?(.*)")
 async def _(event):
@@ -20,7 +21,9 @@ async def _(event):
         await event.edit(str(error_i_a))
         return False
     user_id = replied_user.user.id
-    profile_pic = await event.client.download_profile_photo(user_id, TEMP_DOWNLOAD_DIRECTORY)
+    profile_pic = await event.client.download_profile_photo(
+        user_id, TEMP_DOWNLOAD_DIRECTORY
+    )
     # some people have weird HTML in their names
     first_name = html.escape(replied_user.user.first_name)
     # https://stackoverflow.com/a/5072031/4723940
@@ -34,39 +37,29 @@ async def _(event):
         last_name = html.escape(last_name)
         last_name = last_name.replace("\u2060", "")
     if last_name is None:
-      last_name = "⁪⁬⁮⁮⁮⁮ ‌‌‌‌"
+        last_name = "⁪⁬⁮⁮⁮⁮ ‌‌‌‌"
     # inspired by https://telegram.dog/afsaI181
     user_bio = replied_user.about
     if user_bio is not None:
         user_bio = html.escape(replied_user.about)
-    await bot(functions.account.UpdateProfileRequest(
-        first_name=first_name
-    ))
-    await bot(functions.account.UpdateProfileRequest(
-        last_name=last_name
-    ))
-    await bot(functions.account.UpdateProfileRequest(
-        about=user_bio
-    ))
-    n = 1
-    pfile = await bot.upload_file(profile_pic)  # pylint:disable=E060      
-    await bot(functions.photos.UploadProfilePhotoRequest(  # pylint:disable=E0602
-        pfile
-    ))
-    #message_id_to_reply = event.message.reply_to_msg_id
-    #if not message_id_to_reply:
+    await bot(functions.account.UpdateProfileRequest(first_name=first_name))
+    await bot(functions.account.UpdateProfileRequest(last_name=last_name))
+    await bot(functions.account.UpdateProfileRequest(about=user_bio))
+    pfile = await bot.upload_file(profile_pic)  # pylint:disable=E060
+    await bot(functions.photos.UploadProfilePhotoRequest(pfile))  # pylint:disable=E0602
+    # message_id_to_reply = event.message.reply_to_msg_id
+    # if not message_id_to_reply:
     #    message_id_to_reply = event.message.id
-    #await bot.send_message(
+    # await bot.send_message(
     #  event.chat_id,
     #  "Hey ? Whats Up !",
     #  reply_to=message_id_to_reply,
     #  )
     await event.delete()
     await bot.send_message(
-      event.chat_id,
-      "**KAGEBUNSHIN NO JUTSU**",
-      reply_to=reply_message
-      )
+        event.chat_id, "**KAGEBUNSHIN NO JUTSU**", reply_to=reply_message
+    )
+
 
 async def get_full_user(event):
     if event.reply_to_msg_id:
@@ -74,15 +67,14 @@ async def get_full_user(event):
         if previous_message.forward:
             replied_user = await event.client(
                 GetFullUserRequest(
-                    previous_message.forward.from_id or previous_message.forward.channel_id
+                    previous_message.forward.from_id
+                    or previous_message.forward.channel_id
                 )
             )
             return replied_user, None
         else:
             replied_user = await event.client(
-                GetFullUserRequest(
-                    previous_message.from_id
-                )
+                GetFullUserRequest(previous_message.from_id)
             )
             return replied_user, None
     else:
@@ -122,9 +114,11 @@ async def get_full_user(event):
             except Exception as e:
                 return None, e
 
-CMD_HELP.update({
-    "cloneuser":
-    "\
+
+CMD_HELP.update(
+    {
+        "cloneuser": "\
 .clone <nama pengguna> atau balas ke pesan\
 \nUsage: Salin foto profil target, nama ... dll dan atur sebagai milik Anda."
-})
+    }
+)
