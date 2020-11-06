99951ba3779c957c682ecca93cad678f5313d43d
DarkoBunny <73769985+Ajibcdefgh@users.noreply.github.com>
2020-11-06 12:28:23 +0700

    finally

--- userbot/modules/clone.py
+++ userbot/modules/clone.py
@@ -1,22 +1,14 @@
-"""Get Telegram Profile Picture and other information
-and set as own profile.
-Syntax: .clone @username"""
-# Copy That Plugin by @ViperAdnan
-# modified by @abcdefvvck
-# Give credit if you are going to kang it.
+#Copy That Plugin by @ViperAdnan
+#Give credit if you are going to kang it.
 
 import html
-
-from telethon.tl import functions
+import os
+from telethon.tl.functions.photos import GetUserPhotosRequest
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import MessageEntityMentionName
-
-from userbot import ALIVE_NAME, CMD_HELP, DEFAULT_BIO
+from telethon.tl import functions
+from userbot import CMD_HELP, bot, TEMP_DOWNLOAD_DIRECTORY
 from userbot.events import register
-
-DEFAULTUSER = str(ALIVE_NAME)
-DEFAULTUSERBIO = str(DEFAULT_BIO) if DEFAULT_BIO else "I love @abcdefvvck "
-
 
 @register(outgoing=True, pattern="^\.clone ?(.*)")
 async def _(event):
@@ -28,9 +20,7 @@ async def _(event):
         await event.edit(str(error_i_a))
         return False
     user_id = replied_user.user.id
-    profile_pic = await event.client.download_profile_photo(
-        user_id, Config.TMP_DOWNLOAD_DIRECTORY
-    )
+    profile_pic = await event.client.download_profile_photo(user_id, TEMP_DOWNLOAD_DIRECTORY)
     # some people have weird HTML in their names
     first_name = html.escape(replied_user.user.first_name)
     # https://stackoverflow.com/a/5072031/4723940
@@ -44,49 +34,39 @@ async def _(event):
         last_name = html.escape(last_name)
         last_name = last_name.replace("\u2060", "")
     if last_name is None:
-        last_name = "⁪⁬⁮⁮⁮⁮ ‌‌‌‌"
+      last_name = "⁪⁬⁮⁮⁮⁮ ‌‌‌‌"
     # inspired by https://telegram.dog/afsaI181
     user_bio = replied_user.about
     if user_bio is not None:
-        user_bio = replied_user.about
-    await borg(functions.account.UpdateProfileRequest(first_name=first_name))
-    await borg(functions.account.UpdateProfileRequest(last_name=last_name))
-    await borg(functions.account.UpdateProfileRequest(about=user_bio))
-    pfile = await borg.upload_file(profile_pic)  # pylint:disable=E060
-    await borg(
-        functions.photos.UploadProfilePhotoRequest(pfile)  # pylint:disable=E0602
-    )
-    await event.delete()
-    await borg.send_message(
-        event.chat_id, "**Lu siapa ngab? .. **", reply_to=reply_message
-    )
-    if BOTLOG:
-        await event.client.send_message(
-            BOTLOG_CHATID,
-            f"#CLONED\nSuccesfulley cloned [{first_name}](tg://user?id={user_id })",
-        )
-
-
-@register(outgoing=True, pattern="^\.revert ?(.*)")
-async def _(event):
-    if event.fwd_from:
-        return
-    name = f"{DEFAULTUSER}"
-    bio = f"{DEFAULTUSERBIO}"
+        user_bio = html.escape(replied_user.about)
+    await bot(functions.account.UpdateProfileRequest(
+        first_name=first_name
+    ))
+    await bot(functions.account.UpdateProfileRequest(
+        last_name=last_name
+    ))
+    await bot(functions.account.UpdateProfileRequest(
+        about=user_bio
+    ))
     n = 1
-    await borg(
-        functions.photos.DeletePhotosRequest(
-            await event.client.get_profile_photos("me", limit=n)
-        )
-    )
-    await borg(functions.account.UpdateProfileRequest(about=f"{bio}"))
-    await borg(functions.account.UpdateProfileRequest(first_name=f"{name}"))
-    await event.edit("berhasil dikembalikan ke akun Anda kembali")
-    if BOTLOG:
-        await event.client.send_message(
-            BOTLOG_CHATID, f"#REVERT\nBerhasil dikembalikan ke profil Anda"
-        )
-
+    pfile = await bot.upload_file(profile_pic)  # pylint:disable=E060      
+    await bot(functions.photos.UploadProfilePhotoRequest(  # pylint:disable=E0602
+        pfile
+    ))
+    #message_id_to_reply = event.message.reply_to_msg_id
+    #if not message_id_to_reply:
+    #    message_id_to_reply = event.message.id
+    #await bot.send_message(
+    #  event.chat_id,
+    #  "Hey ? Whats Up !",
+    #  reply_to=message_id_to_reply,
+    #  )
+    await event.delete()
+    await bot.send_message(
+      event.chat_id,
+      "**KAGEBUNSHIN NO JUTSU**",
+      reply_to=reply_message
+      )
 
 async def get_full_user(event):
     if event.reply_to_msg_id:
@@ -94,14 +74,15 @@ async def get_full_user(event):
         if previous_message.forward:
             replied_user = await event.client(
                 GetFullUserRequest(
-                    previous_message.forward.from_id
-                    or previous_message.forward.channel_id
+                    previous_message.forward.from_id or previous_message.forward.channel_id
                 )
             )
             return replied_user, None
         else:
             replied_user = await event.client(
-                GetFullUserRequest(previous_message.from_id)
+                GetFullUserRequest(
+                    previous_message.from_id
+                )
             )
             return replied_user, None
     else:
@@ -110,7 +91,7 @@ async def get_full_user(event):
             input_str = event.pattern_match.group(1)
         except IndexError as e:
             return None, e
-        if event.message.entities:
+        if event.message.entities is not None:
             mention_entity = event.message.entities
             probable_user_mention_entity = mention_entity[0]
             if isinstance(probable_user_mention_entity, MessageEntityMentionName):
@@ -141,13 +122,9 @@ async def get_full_user(event):
             except Exception as e:
                 return None, e
 
-
-CMD_HELP.update(
-    {
-        "clone": ".clone <nama pengguna/balas>\
-\nUsage: mencuri profil orang lain termasuk dp, nama.\
-\n\n.revert\
-\nUsage: Untuk kembali ke profil Anda tetapi ini akan menampilkan ALIVE_NAME sebagai ganti nama Anda saat ini dan DEFAULT_BIO alih-alih bio Anda saat ini\
-"
-    }
-)
+CMD_HELP.update({
+    "cloneuser":
+    "\
+.clone <nama pengguna> atau balas ke pesan\
+\nUsage: Salin foto profil target, nama ... dll dan atur sebagai milik Anda."
+})
