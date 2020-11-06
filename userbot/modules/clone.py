bf4fcaf1ee69d65135c3603b65d4bc54bd8461c0
DarkoBunny <73769985+Ajibcdefgh@users.noreply.github.com>
2020-11-06 11:25:30 +0700

    fix clone

--- userbot/modules/clone.py
+++ userbot/modules/clone.py
@@ -1,16 +1,35 @@
 """Get Telegram Profile Picture and other information
 and set as own profile.
 Syntax: .clone @username"""
+#Copy That Plugin by @ViperAdnan
+#modified by @No_OnE_Kn0wS_Me 
+#Give credit if you are going to kang it.
 
-import asyncio
 import html
-
-from telethon.tl import functions
+import os
+from telethon.tl.functions.photos import GetUserPhotosRequest
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import MessageEntityMentionName
+from telethon.utils import get_input_location
+from userbot.utils import admin_cmd
+from telethon.tl import functions
+from telethon import events
+from telethon.errors import ImageProcessFailedError, PhotoCropSizeSmallError
+from telethon.errors.rpcerrorlist import (PhotoExtInvalidError,
+                                          UsernameOccupiedError)
+from telethon.tl.functions.account import (UpdateProfileRequest,
+                                           UpdateUsernameRequest)
+from telethon.tl.functions.channels import GetAdminedPublicChannelsRequest
+from telethon.tl.functions.photos import (DeletePhotosRequest,
+                                          GetUserPhotosRequest,
+                                          UploadProfilePhotoRequest)
+from telethon.tl.types import InputPhoto, MessageMediaPhoto, User, Chat, Channel
+from userbot import bot, CMD_HELP , AUTONAME , DEFAULT_BIO , ALIVE_NAME
 
-from userbot.utils import CMD_HELP
-
+DEFAULTUSER = str(AUTONAME) if AUTONAME else str(ALIVE_NAME)
+DEFAULTUSERBIO = str(DEFAULT_BIO) if DEFAULT_BIO else "Legends Says To @uSe_DaRk_PrInCe "
+BOTLOG_CHATID = Config.PRIVATE_GROUP_BOT_API_ID
+BOTLOG = True
 
 @borg.on(admin_cmd(pattern="clone ?(.*)"))
 async def _(event):
@@ -22,9 +41,7 @@ async def _(event):
         await event.edit(str(error_i_a))
         return False
     user_id = replied_user.user.id
-    profile_pic = await event.client.download_profile_photo(
-        user_id, Config.TMP_DOWNLOAD_DIRECTORY
-    )
+    profile_pic = await event.client.download_profile_photo(user_id, Config.TMP_DOWNLOAD_DIRECTORY)
     # some people have weird HTML in their names
     first_name = html.escape(replied_user.user.first_name)
     # https://stackoverflow.com/a/5072031/4723940
@@ -38,50 +55,65 @@ async def _(event):
         last_name = html.escape(last_name)
         last_name = last_name.replace("\u2060", "")
     if last_name is None:
-        last_name = "⁪⁬⁮⁮⁮⁮ ‌‌‌‌"
-    # giving myself credits cause y not
+      last_name = "⁪⁬⁮⁮⁮⁮ ‌‌‌‌"
+    # inspired by https://telegram.dog/afsaI181
     user_bio = replied_user.about
-    if user_id == 1263617196:
-        await event.edit("Maaf, tidak bisa mengkloning Dev saya")
-        await asyncio.sleep(3)
-        return
     if user_bio is not None:
-        user_bio = html.escape(replied_user.about)
-    await borg(functions.account.UpdateProfileRequest(first_name=first_name))
-    await borg(functions.account.UpdateProfileRequest(last_name=last_name))
-    await borg(functions.account.UpdateProfileRequest(about=user_bio))
-    pfile = await borg.upload_file(profile_pic)  # pylint:disable=E060
-    await borg(
-        functions.photos.UploadProfilePhotoRequest(pfile)  # pylint:disable=E0602
-    )
-    # message_id_to_reply = event.message.reply_to_msg_id
-    # if not message_id_to_reply:
-    #    message_id_to_reply = event.message.id
-    # await borg.send_message(
-    #  event.chat_id,
-    #  "Hey ? Whats Up !",
-    #  reply_to=message_id_to_reply,
-    #  )
+        user_bio = replied_user.about
+    await borg(functions.account.UpdateProfileRequest(
+        first_name=first_name
+    ))
+    await borg(functions.account.UpdateProfileRequest(
+        last_name=last_name
+    ))
+    await borg(functions.account.UpdateProfileRequest(
+        about=user_bio
+    ))
+    n = 1
+    pfile = await borg.upload_file(profile_pic)  # pylint:disable=E060      
+    await borg(functions.photos.UploadProfilePhotoRequest(  # pylint:disable=E0602
+        pfile
+    ))
     await event.delete()
     await borg.send_message(
-        event.chat_id, "**KAGEBUNSHIN NO JUTSU**", reply_to=reply_message
-    )
-
-
+      event.chat_id,
+      "**Lu siapa ngab? .. **",
+      reply_to=reply_message
+      )
+    if BOTLOG:
+        await event.client.send_message(BOTLOG_CHATID, f"#CLONED\nSuccesfulley cloned [{first_name}](tg://user?id={user_id })")
+    
+@borg.on(admin_cmd(pattern="revert$"))
+async def _(event):
+    if event.fwd_from:
+        return
+    name = f"{DEFAULTUSER}"
+    bio = f"{DEFAULTUSERBIO}"
+    n = 1
+    await borg(functions.photos.DeletePhotosRequest(await event.client.get_profile_photos("me", limit= n)))    
+    await borg(functions.account.UpdateProfileRequest(about=f"{bio}"))
+    await borg(functions.account.UpdateProfileRequest(first_name=f"{name}"))
+    await event.edit("succesfully reverted to your account back")
+    if BOTLOG:
+        await event.client.send_message(BOTLOG_CHATID, f"#REVERT\nSuccesfully reverted back to your profile")
+    
+    
+    
 async def get_full_user(event):
     if event.reply_to_msg_id:
         previous_message = await event.get_reply_message()
         if previous_message.forward:
             replied_user = await event.client(
                 GetFullUserRequest(
-                    previous_message.forward.from_id
-                    or previous_message.forward.channel_id
+                    previous_message.forward.from_id or previous_message.forward.channel_id
                 )
             )
             return replied_user, None
         else:
             replied_user = await event.client(
-                GetFullUserRequest(previous_message.from_id)
+                GetFullUserRequest(
+                    previous_message.from_id
+                )
             )
             return replied_user, None
     else:
@@ -90,7 +122,7 @@ async def get_full_user(event):
             input_str = event.pattern_match.group(1)
         except IndexError as e:
             return None, e
-        if event.message.entities is not None:
+        if event.message.entities:
             mention_entity = event.message.entities
             probable_user_mention_entity = mention_entity[0]
             if isinstance(probable_user_mention_entity, MessageEntityMentionName):
@@ -121,5 +153,10 @@ async def get_full_user(event):
             except Exception as e:
                 return None, e
 
-
-CMD_HELP.update({"cloneuser": ">`.clone`" "\nUsage: meng-kloning orang lain"})
+CMD_HELP.update({
+    "clone":
+    ".clone <nama pengguna/balas>\
+\nUsage: mencuri profil orang lain termasuk dp, nama.\
+\n\n.revert\
+\nUsage: Untuk kembali ke profil Anda tetapi ini akan menampilkan ALIVE_NAME sebagai ganti nama Anda saat ini dan DEFAULT_BIO alih-alih bio Anda saat ini\
+"})
