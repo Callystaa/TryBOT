0b106245f88764647e419c981958ca7c85c99638
DarkoBunny <73769985+Ajibcdefgh@users.noreply.github.com>
2020-12-12 01:28:35 +0000

    pylint: auto fixes

--- userbot/modules/emojigames.py
+++ userbot/modules/emojigames.py
@@ -2,8 +2,9 @@
 # bug fixed by @d3athwarrior
 
 from telethon.tl.types import InputMediaDice
-from userbot.events import register
+
 from userbot import CMD_HELP
+from userbot.events import register
 
 
 @register(outgoing=True, pattern="^.dice(?: |$)(.*)")
@@ -12,13 +13,13 @@ async def _(event):
         return
     input_str = event.pattern_match.group(1)
     await event.delete()
-    r = await event.reply(file=InputMediaDice(''))
+    r = await event.reply(file=InputMediaDice(""))
     if input_str:
         try:
             required_number = int(input_str)
             while not r.media.value == required_number:
                 await r.delete()
-                r = await event.reply(file=InputMediaDice(''))
+                r = await event.reply(file=InputMediaDice(""))
         except BaseException:
             pass
 
@@ -29,13 +30,13 @@ async def _(event):
         return
     input_str = event.pattern_match.group(1)
     await event.delete()
-    r = await event.reply(file=InputMediaDice('🎯'))
+    r = await event.reply(file=InputMediaDice("🎯"))
     if input_str:
         try:
             required_number = int(input_str)
             while not r.media.value == required_number:
                 await r.delete()
-                r = await event.reply(file=InputMediaDice('🎯'))
+                r = await event.reply(file=InputMediaDice("🎯"))
         except BaseException:
             pass
 
@@ -46,18 +47,20 @@ async def _(event):
         return
     input_str = event.pattern_match.group(1)
     await event.delete()
-    r = await event.reply(file=InputMediaDice('🏀'))
+    r = await event.reply(file=InputMediaDice("🏀"))
     if input_str:
         try:
             required_number = int(input_str)
             while not r.media.value == required_number:
                 await r.delete()
-                r = await event.reply(file=InputMediaDice('🏀'))
+                r = await event.reply(file=InputMediaDice("🏀"))
         except BaseException:
             pass
 
-CMD_HELP.update({
-    "emojigames":
-    "`.dice` 1-6 atay `.dart`1-6 atau `.ball`1-5\
+
+CMD_HELP.update(
+    {
+        "emojigames": "`.dice` 1-6 atay `.dart`1-6 atau `.ball`1-5\
 \nUsage: hahaha hanya sebuah keajaiban.\nWarning:`Jangan gunakan nilai lain atau bot akan mogok`"
-})
+    }
+)
