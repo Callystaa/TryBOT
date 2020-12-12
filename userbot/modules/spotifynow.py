c8120070d9607defb4e9df8a5b29889fbf0f54cc
DarkoBunny <73769985+Ajibcdefgh@users.noreply.github.com>
2020-12-12 14:34:24 +0700

    Small initial

--- userbot/modules/spotifynow.py
+++ userbot/modules/spotifynow.py
@@ -9,18 +9,19 @@ from userbot import CMD_HELP, TEMP_DOWNLOAD_DIRECTORY, bot
 from userbot.events import register
 
 
-@register(outgoing=True, pattern=r"^\.spotnow(:? |$)(.*)?")
+@register(outgoing=True, pattern=r"^\.spotnow$")
 async def _(event):
     if event.fwd_from:
         return
     chat = "@SpotifyNowBot"
-    await event.edit("`Processing...`")
+    now = f"/now"
+    await event.edit("`Memproses...`")
     try:
         async with event.client.conversation(chat) as conv:
-            now = "/now"
             try:
                 msg = await conv.send_message(now)
                 response = await conv.get_response()
+                """don't spam notif"""
                 await bot.send_read_acknowledge(conv.chat_id)
             except YouBlockedUserError:
                 await event.reply("`Please unblock` @SpotifyNowBot`...`")
@@ -37,27 +38,29 @@ async def _(event):
                 return
             else:
                 downloaded_file_name = await event.client.download_media(
-                    response.media, TEMP_DOWNLOAD_DIRECTORY
+                    response.media, TEMP_DOWNLOAD_DIRECTORY + "spot.webp"
                 )
                 link = response.reply_markup.rows[0].buttons[0].url
-                await event.client.send_file(
+                spot = await event.client.send_file(
                     event.chat_id,
                     downloaded_file_name,
                     force_document=False,
-                    caption=f"[Play on Spotify]({link})",
                 )
+                await event.respond(
+                    f"[Play on Spotify]({link})", link_preview=False, reply_to=spot
+                )
+                """cleanup chat after completed"""
                 await event.client.delete_messages(conv.chat_id, [msg.id, response.id])
         await event.delete()
         return os.remove(downloaded_file_name)
     except TimeoutError:
-        await event.edit("`@SpotifyNowBot isnt responding..`")
-        await event.client.delete_messages(conv.chat_id, [msg.id])
+        return await event.edit("`Error: `@SpotifyNowBot` is not responding!.`")
 
 
 CMD_HELP.update(
     {
         "spotifynow": ">`.spotnow`"
-        "\nUsage: Show what you're listening on spotify."
-        "\n@SpotifyNowBot"
+        "\nUsage: Tunjukkan apa yang Anda dengarkan di Spotify."
+        "\n`@SpotifyNowBot`"
     }
 )
